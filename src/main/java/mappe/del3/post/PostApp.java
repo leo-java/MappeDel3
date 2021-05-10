e4550241b904978c0f991a6ce9c98916128255f4
Leo <leonardschiller420@gmail.com>
2021-05-11 00:54:12 +0200

    Main view is now openable

--- src/main/java/mappe/del3/post/PostApp.java
+++ src/main/java/mappe/del3/post/PostApp.java
@@ -1,16 +1,33 @@
 package mappe.del3.post;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
 public class PostApp extends Application {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            logger.severe("Error, an IO-Exception has occurred: Cause: " + e.getCause());
+            throw e;
+        }
     }
 }
